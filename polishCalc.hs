bd2e2dc6fb8acdfa902c8645b8be7b1d4a19904a
Ilya Mozerov <ilyailiilia@gmail.com>
2016-10-26 22:04:33 +0300

    added some more functions to calc

--- polishCalc.hs
+++ polishCalc.hs
@@ -1,8 +1,12 @@
 import Data.List
 
-solveRpn :: (Num a, Read a) => String -> a
+solveRpn :: String -> Float
 solveRpn = head . foldl foldingFunction [] . words
         where foldingFunction (x:y:ys) "*" = (x * y):ys
               foldingFunction (x:y:ys) "+" = (x + y):ys
               foldingFunction (x:y:ys) "-" = (y - x):ys
+              foldingFunction (x:y:ys) "/" = (y / x):ys
+              foldingFunction (x:y:ys) "^" = (y ** x):ys
+              foldingFunction (x:xs) "ln" = log x:xs
+              foldingFunction xs "sum" = [sum xs]  
               foldingFunction xs numberString = read numberString:xs
